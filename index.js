dc447949471a5b6a1612bf51692c49c0b4d22df4
LAN_scape <lawrencemueller18@gmail.com>
2020-03-07 23:17:13 -0800

    Added the visuals for the elevator and Queue. I need to push more for this project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const button = document.querySelector(".btn");
 const title = document.querySelector(".title");
 const header = document.querySelector("header");
 const instructions = document.getElementById("instructions");
+const mainContent = document.querySelector(".main-content");
 
 button.addEventListener("click", function(){
     button.classList.toggle("active-btn");
@@ -13,4 +14,5 @@ button.addEventListener("click", function(){
     title.classList.toggle("active-title");
     header.classList.toggle("active-header");
     instructions.classList.toggle("instructions-active");
+    mainContent.classList.toggle("main-content-active");
 });
